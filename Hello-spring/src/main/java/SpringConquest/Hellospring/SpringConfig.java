b4e5f5432b5f788805ee104c48629b437e90d366
HOYA <als77770@naver.com>
2023-03-29 09:50:13 +0900

    스프링 입문
    기초 알아보기 - 1

---
+++ Hello-spring/src/main/java/SpringConquest/Hellospring/SpringConfig.java
@@ -0,0 +1,21 @@
+package SpringConquest.Hellospring;
+
+import SpringConquest.Hellospring.repository.MemberRepository;
+import SpringConquest.Hellospring.repository.MemoryMemberRepository;
+import SpringConquest.Hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+//
+//    @Bean
+//    public MemberService memberService() {
+//        return new MemberService(memberRepository());
+//    }
+//
+//    @Bean
+//    MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
+}
---
+++ Hello-spring/src/test/java/SpringConquest/Hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringConquest.Hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
