86dc035862a18de773e2dfd19ccd2065ce5f5bed
HOYA <102270909+mingeonho1@users.noreply.github.com>
2024-03-27 12:56:05 +0900

    서블릿 Servlet (#4)
    * create .gitignore
    
    * Hello 서블릿
    
    * HttpServletRequest - 기본 사용법
    
    * HTTP 요청 데이터 - GET 쿼리 파라미터
    
    * HTTP 요청 데이터 - POST HTML Form
    
    * HTTP 요청 데이터 - API 메시지 바디 - 단순 텍스트
    
    * HTTP 요청 데이터 - API 메시지 바디 - JSON
    
    * HttpServletResponse - 기본 사용법
    
    * HTTP 응답 데이터 - 단순 텍스트, HTML
    
    * HTTP 응답 데이터 - API JSON

---
+++ servlet/src/main/java/hello/servlet/basic/response/ResponseHtmlServlet.java
@@ -0,0 +1,28 @@
+package hello.servlet.basic.response;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "responseHtmlServlet", urlPatterns = "/response-html")
+public class ResponseHtmlServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// Content-Type: text/html;charset=utf-8
+		resp.setContentType("text/html");
+		resp.setCharacterEncoding("utf-8");
+
+		PrintWriter writer = resp.getWriter();
+		writer.println("<html>");
+		writer.println("<body>");
+		writer.println(" <div>안녕?</div>");
+		writer.println("</body>");
+		writer.println("</html>");
+	}
+}
